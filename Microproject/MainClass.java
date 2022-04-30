30cd7e97ddb4220ec9951d50ae2da9129e143feb
Shambhogit <sjpubgplay@gmail.com>
2022-04-30 18:28:43 +0530

    Added screen shot folder

--- Microproject/MainClass.java
+++ Microproject/MainClass.java
@@ -3,8 +3,19 @@ package com.Microproject;
 import java.util.Scanner;
 
 public class MainClass {
-	private static String admin = "system";
-	private static String pass = "ritp1227";
+	private static String admin1 = "system";
+	private static String pass1 = "ritp1227";
+	
+	private static String admin2 = "shambho";
+	private static String pass2 = "shambho@2003";
+	
+	private static String admin3 = "mandar";
+	private static String pass3 = "mandar@2002";
+	
+	private static String admin4 = "prathamesh";
+	private static String pass4 = "prathamesh@2004";
+	
+
 	public static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
 		
@@ -18,8 +29,11 @@ public class MainClass {
 		String password = sc.next();
 		System.out.println("+----------------------------------------------------------------------------------------------+"); 
 		
-		if(admin.equals(userName) && pass.equals(password)) {
+		if(admin1.equals(userName) && pass1.equals(password)||admin2.equals(userName) && pass2.equals(password)||admin3.equals(userName) && pass3.equals(password)||admin4.equals(userName) && pass4.equals(password)) {
 			System.out.println("Your Loged in Successfully...");
+			System.out.println("Welcome to System : "+userName);
+			System.out.println("+----------------------------------------------------------------------------------------------+"); 
+
 			SwitchClass switchClass = new SwitchClass();
 			switchClass.switchcase();
 			System.out.println("+----------------------------------------------------------------------------------------------+"); 
